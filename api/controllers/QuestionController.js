b09bbb19ff41bae41d88d66a9ae833efc3170693
Andrei Marchenko <andrei.marchenko@rockwool.com>
2019-09-05 15:44:38 +0200

    Fix for removing

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -7,14 +7,12 @@
 
 module.exports = {
 	createMany: function(req,res){
-		// console.log(req.body);
 		Question.createEach(req.body.data)
 		.fetch()
 		.exec((err, data) => {
 			if(err) {
 				console.error(err);
 			}
-			console.log(data);
 			res.json(data);
 		});
 		
